9eafe9a8e8fbd3fc48add65cff1f7830dc286327
Hiren Ray <hirenray882@gmail.com>
2026-07-28 02:25:19 +0530

    change local  path

--- src/services/api/home/jobGet.jsx
+++ src/services/api/home/jobGet.jsx
@@ -1,7 +1,8 @@
 import axios from "axios"
+const endpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
 
 export const getJob = async()=>{
-    const res = await axios.get("http://localhost:3001/api/home/jobs")
+    const res = await axios.get(endpoint+"/api/home/jobs")
     if(!res){
         return
     }
--- src/services/api/home/login.js
+++ src/services/api/home/login.js
@@ -1,7 +1,9 @@
 import axios from "axios"
+const endpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
+
 
 export const LoginNow = async(email,pass)=>{
-    const res = await axios.post("http://localhost:3001/api/home/getway/login",{
+    const res = await axios.post(endpoint+ "/api/home/getway/login",{
         email,pass
     },{
         withCredentials:true,
